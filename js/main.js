3e4cd75670b326f61a1b6f1290f9dc84f1e40c28
Peter LaMalfa <peterlamalfa@Peters-MacBook-Pro.local>
2018-02-03 15:41:44 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2) {
+  if (cards[0] === cards[2]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
