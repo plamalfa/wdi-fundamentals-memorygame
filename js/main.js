7b0997b4e64b80c5b27e27b476c34622a48801cc
Peter LaMalfa <peterlamalfa@Peters-MacBook-Pro.local>
2018-02-05 17:32:23 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,38 +2,50 @@ var cards = [
   {
     rank: "queen",
     suit: "hearts",
-    cardImage: "images/queen-of-hearts.png"
+    cardImage: "images/queen-of-hearts.img"
   },
   {
     rank: "queen",
     suit: "hearts",
-    cardImage: "images/queen-of-diamonds.png"
+    cardImage: "images/queen-of-diamonds.img"
   },
   {
     rank: "king",
     suit: "diamonds",
-    cardImage: "images/king-of-hearts.png"
+    cardImage: "images/king-of-hearts.img"
   },
   {
     rank: "king",
     suit: "diamonds",
-    cardImage: "images/king-of-diamonds.png"
+    cardImage: "images/king-of-diamonds.img"
   }
 ];
 var cardsInPlay = [];
 var checkForMatch = function(){
-  if (cards[0] === cards[2]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
-  }
+  if (cardsInPlay === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+      } else {
+        alert("Sorry, try again.");
+      }
+  };
 }
-var flipCard = function(cardId){
-console.log("User flipped " + (cards[cardId].rank));
+var flipCard = function(){
+var cardId = this.getAttribute("data-id");
+  this.setAttribute("src", cards[cardId].cardImage);
+  console.log("User flipped " + (cards[cardId].rank))
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 cardsInPlay.push(cards[cardId].rank);
-checkForMatch();
-console.log(cards[cardId].cardImage)
-console.log(cards[cardId].suit)
+console.log(checkForMatch());
 };
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.img");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+};
+createBoard ();
